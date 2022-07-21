48f13735f2c7c0bb0c66f9837f47da367fdf2310
Tarun Chandraprakash Ahirwar <codertarun328@gmail.com>
2022-07-21 18:28:21 +0530

    add product details page

--- Frontend/src/Pages/ProductDetails/ProductDetails.tsx
+++ Frontend/src/Pages/ProductDetails/ProductDetails.tsx
@@ -1,7 +1,7 @@
 import './ProductDetails.css';
 
 import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 
 import {
@@ -14,66 +14,132 @@ import {
   Select,
   MenuItem,
   InputLabel,
+  Card,
+  CardActionArea,
+  CardMedia,
 } from '@mui/material';
 
 interface PRODUCT_FORM_DATA {
   ProductName: string;
-  ProductPrice: number;
-  ProductDescription: string;
-  KeyFeatures: string[];
+  Description: string;
+  KeyFeatures: any[];
+  ProductPrice: string;
   ProductImage: string;
-  DiscountedPrice: number;
-  Discount: number;
+  DiscountedPrice: string;
+  Size: string;
   FileUpload: string;
 }
 
 export const ProductDetails = () => {
-  const [keyFeatures, setKeyFeatures] = useState<String[]>([]);
-  const [keyFeature, setKeyFeature] = useState<String>('');
-  const [productDetails, setProductDetails] = useState({
+  // const [keyFeatures, setKeyFeatures] = useState<any[]>([]);
+  // const [keyFeature, setKeyFeature] = useState<String>('');
+  const [productFormData, setProductFormData] = useState<PRODUCT_FORM_DATA>({
     ProductName: '',
-    ProductPrice: '',
-    ProductDescription: '',
+    Description: '',
     KeyFeatures: [],
+    ProductPrice: '',
     ProductImage: '',
     DiscountedPrice: '',
+    Size: '',
+    FileUpload: '',
   });
 
-  const { register, handleSubmit, formState, errors } =
-    useForm<PRODUCT_FORM_DATA>({
-      mode: 'onChange',
+  // setting up the form using use state
+  const [productName, setProductName] = useState('');
+  const [description, setDescription] = useState('');
+  const [singleKeyFeature, setSingleKeyFeature] = useState('');
+  const [keyFeatures, setKeyFeatures] = useState<string[]>([]);
+  const [productPrice, setProductPrice] = useState(0);
+  const [discountedPrice, setDiscountedPrice] = useState(0);
+  const [size, setSize] = useState('');
+  const [productImage, setProductImage] = useState<any[]>([]);
+  const [previewImage, setPreviewImage] = useState<string[]>([]);
+
+  const [active, setActive] = useState(false);
+  const handleSizeChange = (event: any) => {
+    setSize(event.target.value);
+    console.log('size', size);
+  };
+
+  const handleFileUpload = (event: any) => {
+    const files = Array.from(event.target.files);
+
+    setPreviewImage([]);
+    setProductImage([]);
+
+    files.forEach((file: any) => {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        if (reader.readyState === 2) {
+          setPreviewImage((prevState: any) => [...prevState, reader.result]);
+          setProductImage((prevState: any) => [...prevState, reader.result]);
+        }
+      };
+      reader.readAsDataURL(file);
+    });
+    console.log('files', files);
+  };
+
+  // on submit form
+  const onFieldSubmit = (event: any) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+
+    formData.set('ProductName', productName);
+    formData.set('Description', description);
+    formData.set('KeyFeatures', JSON.stringify(keyFeatures));
+    formData.set('ProductPrice', JSON.stringify(productPrice));
+    formData.set('DiscountedPrice', JSON.stringify(discountedPrice));
+    formData.set('Size', JSON.stringify(size));
+
+    productImage.forEach((image: any) => {
+      formData.append('ProductImage', image);
     });
 
-  const onFieldSubmit = async (data: any) => {
-    try {
-    } catch (error) {
-      throw error;
+    for (const [key, value] of formData) {
+      console.log(`${key} = ${value}`);
     }
-  };
-  const handleFieldChange = (target: any) => {
-    // setProductDetails({ ...productDetails, [target.name]: keyFeatures });
-    // console.log('setProductDetails:', setProductDetails);
-  };
-
-  // for Product key features
-  const handleKeyFeatureChange = (target: any) => {
-    setKeyFeature(target.value);
   };
 
   const keyFeatureSubmit = () => {
-    console.log('keyFeatures:', keyFeatures);
-    setKeyFeatures([...keyFeatures, keyFeature]);
-    updatedValues([...keyFeatures, keyFeature]);
+    setKeyFeatures([...keyFeatures, singleKeyFeature]);
+    setSingleKeyFeature('');
   };
 
-  const updatedValues = (latestData: String[]) => {
-    console.log('latestData:', latestData);
+  const discountRate = (price: any, disPrice: any) => {
+    const value = 100 - ((price - disPrice) / price) * 100;
+    return value;
+  };
+
+  const previewChange = () => {
+    setActive(true);
   };
 
   return (
     <div className='flexBox'>
-      <Box className='leftProductBox' />
-      <form onSubmit={handleSubmit(onFieldSubmit)} autoComplete='off'>
+      <Box className='leftProductBox'>
+        {productImage.map((image: any, index: number) => {
+          return (
+            <Card
+              style={active ? { display: 'block' } : { display: 'none' }}
+              className='productImage'
+              key={index}
+            >
+              <CardActionArea>
+                <CardMedia
+                  component='img'
+                  alt='Contemplative Reptile'
+                  height='140'
+                  image={image}
+                  title='Contemplative Reptile'
+                />
+              </CardActionArea>
+            </Card>
+          );
+        })}
+      </Box>
+      <form onSubmit={onFieldSubmit} autoComplete='off'>
         <Box className='productFormBox'>
           <h1>Product Details</h1>
           <Grid
@@ -86,53 +152,38 @@ export const ProductDetails = () => {
               <FormControl variant='outlined' className='formControl mb-1'>
                 <TextField
                   id='product-name'
-                  onChange={(e) => handleFieldChange(e.target)}
+                  onChange={(e) => setProductName(String(e.target.value))}
                   name='ProductName'
+                  value={productName || ''}
                   placeholder='Product Name...'
                   label='Product Name'
                   variant='standard'
-                  inputRef={register({
-                    required: true,
-                  })}
                 />
-                {errors.ProductName && (
-                  <div className='errorMsg'>Enter product name</div>
-                )}
               </FormControl>
               <FormControl variant='outlined' className='formControl mb-1'>
                 <TextField
                   id='Description'
-                  name='Discription'
-                  onChange={(e) => handleFieldChange(e.target)}
+                  name='Description'
+                  value={description || ''}
                   placeholder='Product Description...'
                   label='Product Description'
+                  onChange={(e) => setDescription(e.target.value)}
                   autoComplete='on'
                   variant='standard'
-                  inputRef={register({
-                    required: true,
-                  })}
                 />
-                {errors.ProductDescription && (
-                  <div className='errorMsg'>Enter product discription.</div>
-                )}
               </FormControl>
               <Grid item className='spaceBtw'>
                 <FormControl variant='outlined' className='mb-1'>
                   <TextField
                     id='keyFeatures'
                     name='KeyFeatures'
-                    onChange={(e) => handleKeyFeatureChange(e.target)}
+                    value={singleKeyFeature || ''}
+                    onChange={(e) => setSingleKeyFeature(e.target.value)}
                     placeholder='add product key features...'
                     label='Key Features'
                     autoComplete='on'
                     variant='standard'
-                    inputRef={register({
-                      required: true,
-                    })}
                   />
-                  {errors.KeyFeatures && (
-                    <div className='errorMsg'>Enter product key features.</div>
-                  )}
                 </FormControl>
                 <div className='keyFeatureBtn'>
                   <Button
@@ -149,14 +200,10 @@ export const ProductDetails = () => {
                 <TextField
                   id='price'
                   name='ProductPrice'
+                  value={productPrice || 0}
                   autoComplete='off'
-                  type={'number'}
-                  // onChange={(e) => handleFieldChange(e.target)}
-                  // onChange={(event) =>
-                  //   event.target.value < 0
-                  //     ? (event.target.value = 0)
-                  //     : event.target.value
-                  // }
+                  type='number'
+                  onChange={(e) => setProductPrice(Number(e.target.value))}
                   placeholder='Product Price...'
                   label='MRP'
                   variant='standard'
@@ -165,26 +212,18 @@ export const ProductDetails = () => {
                       <InputAdornment position='end'>₹</InputAdornment>
                     ),
                   }}
-                  inputRef={register({
-                    required: true,
-                  })}
                 />
-                {errors.ProductPrice && (
-                  <div className='errorMsg'>Enter product price.</div>
-                )}
               </FormControl>
               <FormControl variant='outlined' className='formControl mb-1'>
                 <TextField
                   id='discountedPrice'
                   name='DiscountedPrice'
+                  value={discountedPrice}
                   autoComplete='off'
                   type={'number'}
-                  // onChange={(e) => handleFieldChange(e.target)}
-                  // onChange={(event) =>
-                  //   event.target.value < 0
-                  //     ? (event.target.value = 0)
-                  //     : event.target.value
-                  // }
+                  onChange={(e) => {
+                    setDiscountedPrice(Number(e.target.value));
+                  }}
                   placeholder='Discounted Price...'
                   label='Discounted Price'
                   InputProps={{
@@ -193,13 +232,7 @@ export const ProductDetails = () => {
                     ),
                   }}
                   variant='standard'
-                  inputRef={register({
-                    required: true,
-                  })}
                 />
-                {errors.DiscountedPrice && (
-                  <div className='errorMsg'>Enter discounted price.</div>
-                )}
               </FormControl>
               <FormControl variant='outlined' className='formControl mb-1'>
                 <TextField
@@ -207,12 +240,7 @@ export const ProductDetails = () => {
                   name='Discount'
                   autoComplete='off'
                   type={'number'}
-                  // onChange={(e) => handleFieldChange(e.target)}
-                  // onChange={(event) =>
-                  //   event.target.value < 0
-                  //     ? (event.target.value = 0)
-                  //     : event.target.value
-                  // }
+                  value={discountRate(productPrice, discountedPrice)}
                   placeholder='Discount...'
                   label='Discount'
                   InputProps={{
@@ -221,31 +249,27 @@ export const ProductDetails = () => {
                     ),
                   }}
                   variant='standard'
-                  inputRef={register({
-                    required: true,
-                  })}
                 />
-                {errors.DiscountedPrice && (
-                  <div className='errorMsg'>Enter discount.</div>
-                )}
               </FormControl>
               <FormControl variant='standard' className='formControl mb-1'>
                 <InputLabel id='demo-simple-select-standard-label'>
-                  Age
+                  Sizes
                 </InputLabel>
                 <Select
                   labelId='demo-simple-select-standard-label'
                   id='demo-simple-select-standard'
-                  // value={age}
-                  // onChange={handleChange}
-                  label='Age'
+                  value={size}
+                  label='Sizes'
+                  name='Size'
+                  onChange={handleSizeChange}
                 >
-                  <MenuItem value=''>
-                    <em>None</em>
-                  </MenuItem>
-                  <MenuItem value={10}>Ten</MenuItem>
-                  <MenuItem value={20}>Twenty</MenuItem>
-                  <MenuItem value={30}>Thirty</MenuItem>
+                  <MenuItem value={'None'}>None</MenuItem>
+                  <MenuItem value={'X-Small'}>X-Small</MenuItem>
+                  <MenuItem value={'Small'}>Small</MenuItem>
+                  <MenuItem value={'Medium'}>Medium</MenuItem>
+                  <MenuItem value={'Large'}>Large</MenuItem>
+                  <MenuItem value={'X-Large'}>X-Large</MenuItem>
+                  <MenuItem value={'XX-Large'}>XX-Large</MenuItem>
                 </Select>
               </FormControl>
               <FormControl variant='outlined' className='formControl mb-1'>
@@ -255,36 +279,12 @@ export const ProductDetails = () => {
                   type={'file'}
                   label='Upload Image'
                   variant='standard'
-                  inputRef={register({
-                    required: true,
-                  })}
+                  inputProps={{
+                    multiple: true,
+                  }}
+                  onChange={handleFileUpload}
                 />
-                {errors.FileUpload && (
-                  <div className='errorMsg'>Image is required</div>
-                )}
               </FormControl>
-
-              {/* <FormControlLabel
-                className='formControlLabel'
-                control={
-                  <Checkbox
-                    onChange={(e) => handleFieldChange(e.target)}
-                    name='TnC'
-                    color='primary'
-                    inputRef={register({ required: true })}
-                  />
-                }
-                label={
-                  <span className='tncText'>
-                    I agree to the
-                    <a>
-                      <Typography variant='caption' color='primary'>
-                        <a className='TnC'>Terms of User</a>
-                      </Typography>
-                    </a>
-                  </span>
-                }
-              /> */}
             </Grid>
             <div className='registrationBtn'>
               <input
@@ -292,89 +292,13 @@ export const ProductDetails = () => {
                 className='registrationFormBtn'
                 value='Submit'
               />
-              <button className='registrationFormBtn'>Preview Images</button>
+              <button className='registrationFormBtn' onClick={previewChange}>
+                Preview Images
+              </button>
             </div>
           </Grid>
         </Box>
       </form>
     </div>
-
-    // <div className='container'>
-    //   <Box className='leftBoxForm'>
-    //     <Paper variant='outlined' className='formPaper'>
-    //       <form
-    //         onSubmit={handleSubmit(onFieldSubmit)}
-    //         className='formBox'
-    //         autoComplete='off'
-    //       >
-    //         <h1>Product Details</h1>
-    //         <Grid item xs={12}>
-    //           <FormControl className='formControl'>
-    //             <TextField
-    //               name='ProductName'
-    //               label='Product Name'
-    //               className='inputField'
-    //               variant='standard'
-    //               onChange={handleFieldChange}
-    //               inputRef={register({ required: true })}
-    //               error={errors.ProductName ? true : false}
-    //               helperText={
-    //                 errors.ProductName ? 'Product Name is required' : ''
-    //               }
-    //             />
-    //           </FormControl>
-    //         </Grid>
-    //         <Grid item xs={12}>
-    //           <FormControl className='flexBox'>
-    //             <TextField
-    //               name='ProductName'
-    //               label='Product Name'
-    //               variant='standard'
-    //               className='inputField'
-    //               onChange={handleFieldChange}
-    //               inputRef={register({ required: true })}
-    //               error={errors.ProductName ? true : false}
-    //               helperText={
-    //                 errors.ProductName ? 'Product Name is required' : ''
-    //               }
-    //             />
-    //           </FormControl>
-    //         </Grid>
-    //         <Grid item xs={12}>
-    //           <FormControl className='flexBox'>
-    //             <TextField
-    //               name='ProductName'
-    //               label='Product Name'
-    //               variant='standard'
-    //               className='inputField'
-    //               onChange={handleFieldChange}
-    //               inputRef={register({ required: true })}
-    //               error={errors.ProductName ? true : false}
-    //               helperText={
-    //                 errors.ProductName ? 'Product Name is required' : ''
-    //               }
-    //             />
-    //           </FormControl>
-    //         </Grid>
-    //         <Grid item xs={12}>
-    //           <FormControl className='flexBox'>
-    //             <TextField
-    //               name='ProductName'
-    //               label='Product Name'
-    //               variant='standard'
-    //               className='inputField'
-    //               onChange={handleFieldChange}
-    //               inputRef={register({ required: true })}
-    //               error={errors.ProductName ? true : false}
-    //               helperText={
-    //                 errors.ProductName ? 'Product Name is required' : ''
-    //               }
-    //             />
-    //           </FormControl>
-    //         </Grid>
-    //       </form>
-    //     </Paper>
-    //   </Box>
-    // </div>
   );
 };
